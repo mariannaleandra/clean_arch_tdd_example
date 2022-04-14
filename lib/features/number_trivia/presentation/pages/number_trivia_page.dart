db887331d8b4ce9c9908d80c1fb10cbfc32293c2
Marianna <mariannaleandra@gmail.com>
2022-04-14 11:28:25 -0300

    Fix

--- lib/features/number_trivia/presentation/pages/number_trivia_page.dart
+++ lib/features/number_trivia/presentation/pages/number_trivia_page.dart
@@ -24,34 +24,36 @@ class NumberTriviaPage extends StatelessWidget {
       child: Center(
         child: Padding(
           padding: const EdgeInsets.all(10),
-          child: Column(
-            children: <Widget>[
-              const SizedBox(height: 10),
-              // Top half
-              BlocBuilder<NumberTriviaBloc, NumberTriviaState>(
-                builder: (context, state) {
-                  if (state is Empty) {
-                    return const MessageDisplay(
-                      message: 'Start searching!',
-                    );
-                  } else if (state is Loading) {
-                    return const LoadingWidget();
-                  } else if (state is Loaded) {
-                    return TriviaDisplay(
-                      numberTrivia: state.trivia,
-                    );
-                  } else if (state is Error) {
-                    return MessageDisplay(
-                      message: state.message,
-                    );
-                  }
-                  return const SizedBox.shrink();
-                },
-              ),
-              const SizedBox(height: 20),
-              // Bottom half
-              const TriviaControls()
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                const SizedBox(height: 10),
+                // Top half
+                BlocBuilder<NumberTriviaBloc, NumberTriviaState>(
+                  builder: (context, state) {
+                    if (state is Empty) {
+                      return const MessageDisplay(
+                        message: 'Start searching!',
+                      );
+                    } else if (state is Loading) {
+                      return const LoadingWidget();
+                    } else if (state is Loaded) {
+                      return TriviaDisplay(
+                        numberTrivia: state.trivia,
+                      );
+                    } else if (state is Error) {
+                      return MessageDisplay(
+                        message: state.message,
+                      );
+                    }
+                    return const SizedBox.shrink();
+                  },
+                ),
+                const SizedBox(height: 20),
+                // Bottom half
+                const TriviaControls()
+              ],
+            ),
           ),
         ),
       ),
